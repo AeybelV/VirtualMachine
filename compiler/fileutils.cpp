c5da7270415589bb3ed7527340e5473590a5d1f1
AeybelV <aeybelvarghese@gmail.com>
2020-10-04 17:40:14 -0500

    Compiler now adds executable header

--- compiler/fileutils.cpp
+++ compiler/fileutils.cpp
@@ -22,12 +22,21 @@ int readFile(const char *path,std::vector<std::string> *fileContents){
 
 int writeExecutable(std::vector<char> *instructionList){
     std::ofstream compiledExecutable("./output.cke", std::ios::binary);
+
+
     if(!compiledExecutable)
     {
         std::cout << "Compile Error: Could not create file" << std::endl;
         return 1;
     }
 
+    // Writes the executable header
+    compiledExecutable << (char) 0x7E << (char) 0x43 << (char) 0x4B << (char) 0x45; // CakeLang Magic Number
+    compiledExecutable << (char) 0x0A; // 8 bit mode
+    compiledExecutable << '\0'; // Program start as an offset of the header. For now I leave it as 8 bit and starts at offset zero
+    compiledExecutable << (char) 0x43 << (char) 0x4B<< (char) 0x45<< (char) 0x7E; // End of header
+
+    // Writes the actual program
     std::ostream_iterator<char> compiledExecutable_iterator(compiledExecutable);
     std::copy(instructionList->begin(),instructionList->end(),compiledExecutable_iterator);
 
