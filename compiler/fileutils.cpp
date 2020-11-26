8867c0831b746c740601527439ed5489dbf9d700
AeybelV <aeybelvarghese@gmail.com>
2020-11-25 19:56:53 -0600

    A CakeLang executable will no longer have a header

--- compiler/fileutils.cpp
+++ compiler/fileutils.cpp
@@ -30,13 +30,6 @@ int writeExecutable(std::vector<char> *instructionList){
         return 1;
     }
 
-    // Writes the executable header
-    compiledExecutable << (char) 0x7E << (char) 0x43 << (char) 0x4B << (char) 0x45; // CakeLang Magic Number
-    compiledExecutable << (char) 0x0A; // 8 bit mode
-    compiledExecutable << '\0'; // Program start as an offset of the header. For now I leave it as 8 bit and starts at offset zero
-    compiledExecutable << (char) 0x43 << (char) 0x4B<< (char) 0x45<< (char) 0x7E; // End of header
-
-    // Writes the actual program
     std::ostream_iterator<char> compiledExecutable_iterator(compiledExecutable);
     std::copy(instructionList->begin(),instructionList->end(),compiledExecutable_iterator);
 
